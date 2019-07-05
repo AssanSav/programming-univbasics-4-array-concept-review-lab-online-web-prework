c6dd4b6cc8d42f300f09fdf417b08c039826d7f0
assane savadogo <assane.savadogo81@gmail.com>
2019-07-05 18:26:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,5 @@
 def find_element_index(array, value_to_find)
-  return array.index(value_to_find) {|index|
-return index
+  return array.index(value_to_find) {|index| return index}
 end
 
 def find_max_value(array)
